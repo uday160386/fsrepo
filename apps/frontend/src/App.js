97b4813f60224d8892e344b1a1426c7761f65663
udaykumar-dragon <udaykumar.bkvenkata@projectdrgn.com>
2020-08-22 07:03:55 +0530

    first commit

---
+++ apps/frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import {Greeting} from './Greetings';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Greeting />
+      </header>
+    </div>
+  );
+}
+
+export default App;
