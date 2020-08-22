c3c11f8da9d01619b8233087dde17f647dfdb594
uday160386 <uday.160386@gmail.com>
2020-08-22 14:28:40 +0530

    Fixed the project issues

---
+++ apps/frontend/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {Greeting} from './Greeting';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <Greeting />
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
