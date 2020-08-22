e0103213a3f982d9281074d18b3f9c3a10f7361a
uday160386 <uday.160386@gmail.com>
2020-08-22 07:13:28 +0530

    clean

--- packages/validator/index.js
+++
@@ -1,5 +0,0 @@
-/**
- * Checks if given value is null or undefined or whitespace string
- * @param {string?} value 
- */
-exports.isNullOrWhitespace = (value) => value === undefined || value === null || !value.trim();
